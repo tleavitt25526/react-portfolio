55c66390a1ae9d439edbe27cc2066e360dbd3bd4
tleavitt25526 <tleavitt25526@gmail.com>
2024-10-23 10:28:30 -0400

    App.css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-//import './App.css'
+import './App.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
